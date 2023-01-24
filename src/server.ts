5c55a8c8b8b2e7bbc073b36ef29744ff9a98bb22
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2023-01-24 19:21:16 -0300

    Criando a rota task, adicionando crud da task, criando middleware e validações com zod

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
+import { tasksRouter } from './routes/tasks'
 import { loginRouter } from './routes/login'
 import { registerRouter } from './routes/register'
+import { authMiddleware } from './middleware/auth'
 
 dotenv.config()
 const app = express()
@@ -11,6 +13,9 @@ app.use(cors())
 app.use(express.json())
 app.use(registerRouter)
 app.use(loginRouter)
+
+app.use(authMiddleware)
+app.use(tasksRouter)
 
 app.listen(process.env.PORT || 3333, () => {
 	console.log('Server is running on port 3333')
