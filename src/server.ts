d2a76e8556c4b263515332467046b9745d7c602b
matheusgmc <sdmastergames@hotmail.com>
2023-01-24 21:46:52 -0400

    adicionando a rota da doc no express

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
+import { swaggerRouter } from './routes/docs'
 import { loginRouter } from './routes/login'
 import { registerRouter } from './routes/register'
 
@@ -11,7 +12,8 @@ app.use(cors())
 app.use(express.json())
 app.use(registerRouter)
 app.use(loginRouter)
+app.use(swaggerRouter)
 
 app.listen(process.env.PORT || 3333, () => {
-	console.log('Server is running on port 3333')
+  console.log('Server is running on port 3333')
 })
